5a8dbc7c83183574332e4e308dae71ff35d4f6f3
Will Jameson <wbjame83@gmail.com>
2024-06-14 19:16:40 -0700

    new view

---
+++ git_pracice/Second_view.swift
@@ -0,0 +1,18 @@
+//
+//  Second_view.swift
+//  git_pracice
+//
+//  Created by Will Jameson on 6/14/24.
+//
+
+import SwiftUI
+
+struct Second_view: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Second_view()
+}
