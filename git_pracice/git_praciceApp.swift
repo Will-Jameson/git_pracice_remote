b28d2adafb77fc18a078229cb560f3d0c072d21a
Will Jameson <wbjame83@gmail.com>
2024-06-14 18:52:54 -0700

    Initial Commit

---
+++ git_pracice/git_praciceApp.swift
@@ -0,0 +1,17 @@
+//
+//  git_praciceApp.swift
+//  git_pracice
+//
+//  Created by Will Jameson on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct git_praciceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
